7a02b0b8988e9cc33d641ccc78ff69e599343e10
tekkiwebsolutions <sarbjitgrewal@tekkiwebsolutions.com>
2021-10-09 01:15:42 +0530

    pages completed

---
+++ src/components/JustTextSnippetPost/JustTextSnippetPost.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './JustTextSnippetPost.css';
+
+function JustTextSnippetPost(){
+
+    return(
+        <div className='just_txt'>
+                <h2>Lorem Ipsum is simply dummy</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy </p>
+                <a href='#'>Read More</a>
+            </div>
+    )
+}
+export default JustTextSnippetPost;
